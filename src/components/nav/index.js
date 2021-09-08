9f6f91c7ae0070851751f9768a00c26d3e1158c6
Elijah Miernicki <miernickielijah@gmail.com>
2021-09-08 18:51:47 -0500

    added nav and component to render on app.js

---
+++ src/components/nav/index.js
@@ -0,0 +1,15 @@
+function Nav () {
+
+    return ( <nav id="navbar">
+    <ul id="navs">
+        <li class="hide delay1 tab active" id="fast"><a href="../Swashbuckler/index.html">HOME</a></li>
+        <li class="hide delay2 tab" id="fast"><a href="../Swashbuckler/bio.html">BIO</a></li>
+        <li class="hide delay3 tab" id="fast"><a href="../Swashbuckler/projects.html">PROJECTS</a></li>
+        <li class="hide delay4 tab" id="fast"><a href="../Swashbuckler/contact.html">CONTACT</a></li>
+        <li class="hide delay5 tab" id="fast"><a href="../Swashbuckler/services.html">SERVICES</a></li>
+    </ul>
+</nav>)
+
+}
+
+export default Nav; 
